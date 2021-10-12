f9afb5ab4769c62455792f1e9cbea0291624c5b3
hugo@DESKTOP-J7IENLF <cristian260893@gmail.com>
2021-10-12 14:43:27 -0500

    Protección de Rutas

--- src/app/modules/auth/pages/login-page/login-page.component.ts
+++ src/app/modules/auth/pages/login-page/login-page.component.ts
@@ -1,31 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.css']
+  styleUrls: ['./login-page.component.css'],
 })
 export class LoginPageComponent implements OnInit {
   formLogin: FormGroup = new FormGroup({});
-  errorSession: any;
-  constructor(private _authService: AuthService) {
+  errorSession: boolean = false;
+  constructor(
+    private _authService: AuthService,
+    private cookieService: CookieService,
+    private router: Router
+  ) {
     this.formLogin = new FormGroup({
-      email: new FormControl('', [
-        Validators.required, Validators.email
-      ]),
+      email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [
-        Validators.required, Validators.minLength(6),
-        Validators.maxLength(12)
-      ])
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(12),
+      ]),
     });
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
   sendLogin() {
     const { email, password } = this.formLogin.value;
     this._authService.sendCredentials(email, password)
+      .subscribe(
+        (
+          responseOk // Cuando el usuario ingresa sus credenciales Correctas
+        ) => {
+          const { tokenSession, data } = responseOk;
+          this.cookieService.set('token', tokenSession, 4, '/');
+          this.router.navigate(['/', 'tracks'])
+        },
+        (error) => {
+          this.errorSession = true;
+          console.log(error);
+        }
+      );
   }
 }
--- src/app/modules/auth/services/auth.service.ts
+++ src/app/modules/auth/services/auth.service.ts
@@ -1,12 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
+  private readonly URL = environment.api;
   constructor(private http: HttpClient) { }
 
-  sendCredentials(email: string, password: string): void { }
+  sendCredentials(email: string, password: string): Observable<any> {
+    const body = {
+      email,
+      password
+    }
+    return this.http.post(`${this.URL}/auth/login`, body)
+  }
 }
